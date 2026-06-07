c4efc4e754cbebcc53d5541c39dd4822d1d79ee7
proyectpanda <proyectpanda@gmail.com>
2026-06-07 21:28:24 +0200

    Mount intro audio once per session

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { PlayersProvider } from "./context/PlayersContext";
+import { IntroAudio } from "./components/IntroAudio";
 
 export default function App() {
   return (
     <PlayersProvider>
+      <IntroAudio />
       <RouterProvider router={router} />
     </PlayersProvider>
   );
