eb75b2962c2ba1dbf06f7d261ab65fdcee06af86
proyectpanda <proyectpanda@gmail.com>
2026-06-21 23:23:58 +0200

    Add reports page route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,6 +4,7 @@ import { OKampaniiPage } from "./components/OKampaniiPage";
 import { GraczePage } from "./components/GraczePage";
 import { RegulaminPage } from "./components/RegulaminPage";
 import { ZasadyPage } from "./components/ZasadyPage";
+import { RaportyPage } from "./components/RaportyPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/dolacz" replace /> },
@@ -12,5 +13,6 @@ export const router = createBrowserRouter([
   { path: "/zasady", Component: ZasadyPage },
   { path: "/regulamin", Component: RegulaminPage },
   { path: "/gracze", Component: GraczePage },
+  { path: "/raporty", Component: RaportyPage },
   { path: "*", element: <Navigate to="/dolacz" replace /> },
 ]);
