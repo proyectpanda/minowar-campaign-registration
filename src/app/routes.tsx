04a124b3298a7741b420ea371b5ea3f8bdab5c8a
proyectpanda <proyectpanda@gmail.com>
2026-06-21 22:10:47 +0200

    Add rules page route

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,11 +3,13 @@ import { DolaczPage } from "./components/DolaczPage";
 import { OKampaniiPage } from "./components/OKampaniiPage";
 import { GraczePage } from "./components/GraczePage";
 import { RegulaminPage } from "./components/RegulaminPage";
+import { ZasadyPage } from "./components/ZasadyPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/dolacz" replace /> },
   { path: "/dolacz", Component: DolaczPage },
   { path: "/kampania", Component: OKampaniiPage },
+  { path: "/zasady", Component: ZasadyPage },
   { path: "/regulamin", Component: RegulaminPage },
   { path: "/gracze", Component: GraczePage },
   { path: "*", element: <Navigate to="/dolacz" replace /> },
